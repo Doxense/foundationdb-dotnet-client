3f8286dce9bfe9c7215cefa034f8a9a745b431fe
Christophe Chevalier <chevalier@doxense.com>
2013-03-11 10:00:45 +0100

    Cleaned up sandbox, added some more benchmarks

--- FoundationDb.Tests.Sandbox/Program.cs
+++ FoundationDb.Tests.Sandbox/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Linq;
 
 namespace FoundationDb.Tests.Sandbox
 {
@@ -31,10 +32,207 @@ namespace FoundationDb.Tests.Sandbox
 			Console.ReadKey();
 		}
 
+		static async Task TestSimpleTransactionAsync(FdbDatabase db)
+		{
+			Console.WriteLine("Starting new transaction...");
+			using (var trans = db.BeginTransaction())
+			{
+				Console.WriteLine("> Transaction ready");
+
+				Console.WriteLine("Getting read version...");
+				var readVersion = await trans.GetReadVersion();
+				Console.WriteLine("> Read Version = " + readVersion);
+
+				Console.WriteLine("Getting 'hello'...");
+				var result = await trans.GetAsync("hello");
+				//var result = trans.Get("hello");
+				if (result == null)
+					Console.WriteLine("> hello NOT FOUND");
+				else
+					Console.WriteLine("> hello = " + Encoding.UTF8.GetString(result));
+
+				Console.WriteLine("Setting 'Foo' = 'Bar'");
+				trans.Set("Foo", "Bar");
+
+				Console.WriteLine("Setting 'TopSecret' = rnd(512)");
+				var data = new byte[512];
+				new Random(1234).NextBytes(data);
+				trans.Set("TopSecret", data);
+
+				Console.WriteLine("Committing transaction...");
+				await trans.CommitAsync();
+				//trans.Commit();
+				Console.WriteLine("> Committed!");
+			}
+		}
+
+		static async Task BenchInsertSmallKeysAsync(FdbDatabase db, int N)
+		{
+			// insert a lot of small keys, in a single transaction
+
+			var sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				for (int i = 0; i < N; i++)
+				{
+					trans.Set("hello" + i, "world" + i);
+				}
+				await trans.CommitAsync();
+			}
+			sw.Stop();
+			Console.WriteLine("Took " + sw.Elapsed + " to insert " + N + " items (" + (sw.Elapsed.TotalMilliseconds / N) + "/write)");
+		}
+
+		static async Task BenchSerialReadAsync(FdbDatabase db, int N)
+		{
+			// read a lot of small keys, one by one
+
+			var sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				for (int i = 0; i < N; i++)
+				{
+					var result = await trans.GetAsync("hello" + i);
+				}
+			}
+			sw.Stop();
+			Console.WriteLine("Took " + sw.Elapsed + " to read " + N + " items (" + (sw.Elapsed.TotalMilliseconds / N) + "/read)");
+		}
+
+		static async Task BenchConcurrentReadAsync(FdbDatabase db, int N)
+		{
+			// read a lot of small keys, concurrently
+
+			var sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				var results = await Task.WhenAll(Enumerable
+					.Range(0, N)
+					.Select((i) => trans.GetAsync("hello" + i))
+				);
+			}
+			sw.Stop();
+			Console.WriteLine("Took " + sw.Elapsed + " to read " + N + " items (" + (sw.Elapsed.TotalMilliseconds / N) + "/read)");
+
+			var keys = Enumerable.Range(0, N).Select(i => "hello" + i).ToArray();
+
+			sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				var results = await trans.GetBatchAsync(keys);
+			}
+			sw.Stop();
+			Console.WriteLine("Took " + sw.Elapsed + " to read " + keys.Length + " items (" + (sw.Elapsed.TotalMilliseconds / keys.Length) + "/read)");
+		}
+
+		static void BenchSerialReadBlocking(FdbDatabase db, int N)
+		{
+			// read a lot of small keys, by blocking (no async)
+
+			var sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				for (int i = 0; i < N; i++)
+				{
+					var result = trans.Get("hello" + i);
+				}
+			}
+			sw.Stop();
+			Console.WriteLine("Took " + sw.Elapsed + " to read " + N + " items (" + (sw.Elapsed.TotalMilliseconds / N) + "/read)");
+		}
+
+		static async Task BenchClearAsync(FdbDatabase db, int N)
+		{
+			// clear a lot of small keys, in a single transaction
+
+			var sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				for (int i = 0; i < N; i++)
+				{
+					trans.Clear("hello" + i);
+				}
+
+				await trans.CommitAsync();
+			}
+			sw.Stop();
+			Console.WriteLine("Took " + sw.Elapsed + " to clear " + N + " items (" + (sw.Elapsed.TotalMilliseconds / N) + "/write)");
+		}
+
+		static async Task BenchUpdateSameKeyLotsOfTimesAsync(FdbDatabase db, int N)
+		{
+			// continuously update same key by adding a little bit more
+
+			var list = new byte[N];
+			var update = Stopwatch.StartNew();
+			for (int i = 0; i < N; i++)
+			{
+				list[i] = (byte)i;
+				using (var trans = db.BeginTransaction())
+				{
+					trans.Set("list", list);
+					await trans.CommitAsync();
+				}
+			}
+			update.Stop();
+
+			Console.WriteLine("Took " + update.Elapsed + " to fill a byte[" + N + "] one by one (" + (update.Elapsed.TotalMilliseconds / N) + "/write)");
+		}
+
+		static async Task BenchUpdateLotsOfKeysAsync(FdbDatabase db, int N)
+		{
+			// continuously update same key by adding a little bit more
+
+			var keys = Enumerable.Range(0, N).Select(x => "list" + x.ToString()).ToArray();
+
+			Console.WriteLine("> creating " + N + " half filled keys");
+			var segment = new byte[60];
+
+			for (int i = 0; i < (segment.Length >> 1); i++) segment[i] = (byte)(i >> 2);
+			using (var trans = db.BeginTransaction())
+			{
+				for (int i = 0; i < N; i++)
+				{
+					trans.Set(keys[i], segment);
+				}
+				await trans.CommitAsync();
+			}
+
+			Console.WriteLine("Changing one byte in each of the " + N + " keys...");
+			var sw = Stopwatch.StartNew();
+			using (var trans = db.BeginTransaction())
+			{
+				Console.WriteLine("READ");
+				// get all the lists
+				var data = await trans.GetBatchAsync(keys);
+
+				// change them
+				Console.WriteLine("CHANGE");
+				for (int i = 0; i < data.Count; i++)
+				{
+					var list = data[i].Value;
+					list[(list.Length >> 1) + 1] = (byte)i;
+					trans.Set(data[i].Key, list);
+				}
+
+				Console.WriteLine("COMMIT");
+				await trans.CommitAsync();
+			}
+			sw.Stop();
+
+			Console.WriteLine("Took " + sw.Elapsed + " to change a byte in " + N + " lists (" + (sw.Elapsed.TotalMilliseconds / N) + "/write)");
+
+		}
+
 		static async Task MainAsync(string[] args)
 		{
-			FdbCore.NativeLibPath = @"C:\Program Files\foundationdb\bin";
-			FdbCore.TracePath = Path.Combine(Path.GetTempPath(), "fdb");
+			const int N = 1000;
+			const string NATIVE_PATH = @"C:\Program Files\foundationdb\bin";
+
+			FdbCore.NativeLibPath = NATIVE_PATH;
+
+			// uncomment this to enable network thread tracing
+			//FdbCore.TracePath = Path.Combine(Path.GetTempPath(), "fdb");
 
 			int apiVersion = FdbCore.GetMaxApiVersion();
 			Console.WriteLine("Max API Version: " + apiVersion);
@@ -46,7 +244,7 @@ namespace FoundationDb.Tests.Sandbox
 				Console.WriteLine("> Up and running");
 
 				Console.WriteLine("Connecting to local cluster...");
-				using (var cluster = await FdbCore.ConnectAsync(null))
+				using (var cluster = await FdbCore.OpenLocalClusterAsync())
 				{
 					Console.WriteLine("> Connected!");
 
@@ -55,103 +253,21 @@ namespace FoundationDb.Tests.Sandbox
 					{
 						Console.WriteLine("> Connected to db '{0}'", db.Name);
 
-						Console.WriteLine("Starting new transaction...");
-						using (var trans = db.BeginTransaction())
-						{
-							Console.WriteLine("> Transaction ready");
+						//await TestSimpleTransactionAsync(db);
 
-							Console.WriteLine("Getting read version...");
-							var readVersion = await trans.GetReadVersion();
-							Console.WriteLine("> Read Version = " + readVersion);
+						//await BenchInsertSmallKeysAsync(db, N);
 
-							Console.WriteLine("Getting 'hello'...");
-							var result = await trans.GetAsync("hello");
-							//var result = trans.Get("hello");
-							if (result == null)
-								Console.WriteLine("> hello NOT FOUND");
-							else
-								Console.WriteLine("> hello = " + Encoding.UTF8.GetString(result));
+						//await BenchSerialReadAsync(db, N);
 
-							Console.WriteLine("Setting 'Foo' = 'Bar'");
-							trans.Set("Foo", "Bar");
+						//await BenchConcurrentReadAsync(db, N);
 
-							Console.WriteLine("Setting 'TopSecret' = rnd(512)");
-							var data = new byte[512];
-							new Random(1234).NextBytes(data);
-							trans.Set("TopSecret", data);
+						//BenchSerialReadBlocking(db, N);
 
-							Console.WriteLine("Committing transaction...");
-							await trans.CommitAsync();
-							//trans.Commit();
-							Console.WriteLine("> Committed!");
-						}
+						//await BenchClearAsync(db, N);
 
-					const int N = 1000;
+						//await BenchUpdateSameKeyLotsOfTimesAsync(db, N);
 
-					#region INSERT...
-
-					var insert = Stopwatch.StartNew();
-					using (var trans = db.BeginTransaction())
-					{
-						for (int i = 0; i < N; i++)
-						{
-							trans.Set("hello" + i, "world" + i);
-						}
-						await trans.CommitAsync();
-					}
-					insert.Stop();
-					Console.WriteLine("Took " + insert.Elapsed + " to insert " + N + " items (" + (insert.Elapsed.TotalMilliseconds / N) + "/write)");
-
-					#endregion
-
-					#region READ ASYNC
-
-					var read = Stopwatch.StartNew();
-					using (var trans = db.BeginTransaction())
-					{
-						for (int i = 0; i < N; i++)
-						{
-							var result = await trans.GetAsync("hello"+ i);
-						}
-						await trans.CommitAsync();
-					}
-					read.Stop();
-					Console.WriteLine("Took " + read.Elapsed + " to read " + N + " items (" + (read.Elapsed.TotalMilliseconds / 1000) + "/read)");
-
-					#endregion
-
-					#region READ BLOCKING
-
-					var read2 = Stopwatch.StartNew();
-					using (var trans = db.BeginTransaction())
-					{
-						for (int i = 0; i < N; i++)
-						{
-							var result = trans.Get("hello" + i);
-						}
-						await trans.CommitAsync();
-					}
-					read2.Stop();
-					Console.WriteLine("Took " + read2.Elapsed + " to read " + N + " items (" + (read2.Elapsed.TotalMilliseconds / 1000) + "/read)");
-
-					#endregion
-
-					#region CLEAR
-
-					var clear = Stopwatch.StartNew();
-					using (var trans = db.BeginTransaction())
-					{
-						for (int i = 0; i < N; i++)
-						{
-							trans.Clear("hello" + i);
-						}
-
-						await trans.CommitAsync();
-					}
-					clear.Stop();
-					Console.WriteLine("Took " + clear.Elapsed + " to clear " + N + " items (" + (clear.Elapsed.TotalMilliseconds / 1000) + "/write)");
-
-					#endregion
+						await BenchUpdateLotsOfKeysAsync(db, N);
 
 						Console.WriteLine("time to say goodbye...");
 					}
