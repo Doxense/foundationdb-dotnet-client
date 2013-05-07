0e9a119af5286c8b8c68734b317e2e875dbb3765
Christophe Chevalier <chevalier@doxense.com>
2013-05-07 15:02:12 +0200

    Quick porting of string interning layer
    * Added FdbStringIntern class
    * Added test

---
+++ FoundationDb.Client/Layers/Strings/FdbStringIntern.cs
@@ -0,0 +1,250 @@
+﻿#region BSD Licence
+/* Copyright (c) 2013, Doxense SARL
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of the <organization> nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#endregion
+
+using FoundationDb.Client.Tuples;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoundationDb.Client.Tables
+{
+
+	public class FdbStringIntern
+	{
+		// Differences with the stringintern.py implementation at https://github.com/FoundationDB/python-layers/blob/master/lib/stringintern.py
+		// * UID are byte[] but we store them as a base64 encoded string in the in-memory dictionary. They are still stored as byte[] in the tuple
+
+		private static readonly string StringUidPrefix = "S";
+		private static readonly string UidStringPrefix = "U";
+		private const int CacheLimitBytes = 10 * 1000 * 1000;
+
+		private readonly List<string> m_uidsInCache = new List<string>();
+		private readonly Dictionary<string, string> m_uidStringCache = new Dictionary<string, string>();
+		private readonly Dictionary<string, string> m_stringUidCache = new Dictionary<string, string>();
+		private int m_bytesCached;
+
+		private readonly Random m_rnd = new Random();
+		private readonly RandomNumberGenerator m_prng = RandomNumberGenerator.Create();
+
+		private readonly ReaderWriterLockSlim m_lock = new ReaderWriterLockSlim();
+
+		public FdbStringIntern(FdbDatabase database, FdbSubspace subspace)
+		{
+			if (database == null) throw new ArgumentNullException("database");
+			if (subspace == null) throw new ArgumentNullException("subspace");
+
+			this.Database = database;
+			this.Subspace = subspace;
+		}
+
+		public FdbDatabase Database { get; private set; }
+
+		public FdbSubspace Subspace { get; private set; }
+
+		public IFdbTuple UidKey(byte[] uid)
+		{
+			return this.Subspace.Append(UidStringPrefix, uid);
+		}
+
+		public IFdbTuple StringKey(string value)
+		{
+			return this.Subspace.Append(StringUidPrefix, value);
+		}
+
+		private void EvictCache()
+		{
+			if (m_uidsInCache.Count == 0)
+			{
+				throw new InvalidOperationException("Cannot evict from empty cache");
+			}
+
+			// Random eviction
+			int i = m_rnd.Next(m_uidsInCache.Count);
+
+			// remove from uids_in_cache
+			string uidKey = m_uidsInCache[i];
+			m_uidsInCache[i] = m_uidsInCache[m_uidsInCache.Count - 1];
+			m_uidsInCache.RemoveAt(m_uidsInCache.Count - 1);
+
+			// remove from caches, account for bytes
+			string value;
+			if (!m_uidStringCache.TryGetValue(uidKey, out value) || value == null)
+			{
+				throw new InvalidOperationException("Error in cache evication: string not found");
+			}
+
+			m_uidStringCache.Remove(uidKey);
+			m_stringUidCache.Remove(value);
+
+			int size = (value.Length + uidKey.Length) * 2;
+			Interlocked.Add(ref m_bytesCached, -size);
+		}
+
+		private void AddToCache(string value, byte[] uid)
+		{
+			while (m_bytesCached > CacheLimitBytes)
+			{
+				EvictCache();
+			}
+
+			string uidKey = Convert.ToBase64String(uid);
+
+			m_lock.EnterUpgradeableReadLock();
+			try
+			{
+				if (!m_uidStringCache.ContainsKey(uidKey))
+				{
+
+					m_lock.EnterWriteLock();
+					try
+					{
+						m_stringUidCache[value] = uidKey;
+						m_uidStringCache[uidKey] = value;
+						m_uidsInCache.Add(uidKey);
+					}
+					finally
+					{
+						m_lock.ExitWriteLock();
+					}
+
+					int size = (value.Length + uidKey.Length) * 2;
+					Interlocked.Add(ref m_bytesCached, size);
+
+				}
+			}
+			finally
+			{
+				m_lock.ExitUpgradeableReadLock();
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="trans"></param>
+		/// <returns></returns>
+		private async Task<byte[]> FindUidAsync(FdbTransaction trans)
+		{
+			// note: we diverge from stringingern.py here by converting the UID (bytes) into Base64 in the cache.
+			// this allows us to use StringComparer.Ordinal as a comparer for the Dictionary<K, V> and not EqualityComparer<byte[]>
+
+			int tries = 0;
+			while (true)
+			{
+				var bytes = new byte[4 + tries];
+				m_prng.GetBytes(bytes);
+
+				if (m_uidStringCache.ContainsKey(Convert.ToBase64String(bytes)))
+					continue;
+
+				if (await trans.GetAsync(UidKey(bytes)) == null)
+					return bytes;
+
+				++tries;
+			}
+		}
+
+		/// <summary>Look up string <paramref name="value"/> in the intern database and return its normalized representation. If value already exists, intern returns the existing representation.</summary>
+		/// <param name="trans">Fdb transaction</param>
+		/// <param name="value">String to intern</param>
+		/// <returns>Normalized representation of the string</returns>
+		/// <remarks><paramref name="value"/> must fit within a FoundationDB value</remarks>
+		public Task<byte[]> InternAsync(FdbTransaction trans, string value)
+		{
+			Debug.WriteLine("Want to intern: " + value);
+			string uidKey;
+
+			if (m_stringUidCache.TryGetValue(value, out uidKey))
+			{
+				Debug.WriteLine("> found in cache! " + uidKey);
+				return Task.FromResult(Convert.FromBase64String(uidKey));
+			}
+
+			Debug.WriteLine("_ not in cache, taking slow route...");
+
+			return InternSlowAsync(trans, value);
+		}
+
+		private async Task<byte[]> InternSlowAsync(FdbTransaction trans, string value)
+		{
+			var stringKey = StringKey(value);
+
+			byte[] uid = await trans.GetAsync(stringKey);
+			if (uid == null)
+			{
+				Debug.WriteLine("_ not found in db, will create...");
+
+				uid = await FindUidAsync(trans);
+				Debug.WriteLine("> using new uid " + Convert.ToBase64String(uid));
+
+				trans.Set(UidKey(uid), Encoding.UTF8.GetBytes(value));
+				trans.Set(stringKey, uid);
+
+				AddToCache(value, uid);
+			}
+			else
+			{
+				Debug.WriteLine("> found in db with uid " + Convert.ToBase64String(uid));
+			}
+			return uid;
+		}
+
+		/// <summary>Return the long string associated with the normalized representation <paramref name="uid"/></summary>
+		/// <param name="trans"></param>
+		/// <param name="uid"></param>
+		/// <returns></returns>
+		public Task<string> LookupAsync(FdbTransaction trans, byte[] uid)
+		{
+			string value;
+			if (m_uidStringCache.TryGetValue(Convert.ToBase64String(uid), out value))
+			{
+				return Task.FromResult(value);
+			}
+
+			return LookupSlowAsync(trans, uid);
+		}
+
+		private async Task<string> LookupSlowAsync(FdbTransaction trans, byte[] uid)
+		{
+			byte[] valueBytes = await trans.GetAsync(UidKey(uid));
+			if (valueBytes == null) throw new KeyNotFoundException("String intern indentifier not found");
+
+			string value = Encoding.UTF8.GetString(valueBytes);
+			AddToCache(value, uid);
+
+			return value;
+		}
+
+	}
+
+}
---
+++ FoundationDb.Tests/Layers/StringInternFacts.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FoundationDb.Client;
+using System.Threading.Tasks;
+using System.Threading;
+using FoundationDb.Client.Tuples;
+using FoundationDb.Client.Tables;
+using System.Diagnostics;
+
+namespace FoundationDb.Tests
+{
+
+	[TestFixture]
+	public class StringInternFacts
+	{
+
+		[Test]
+		public async Task Test_StringIntern_Example()
+		{
+			using(var db = await Fdb.OpenLocalDatabaseAsync("DB"))
+			{
+				var location = new FdbSubspace("BigStrings");
+				var strs = new FdbStringIntern(db, location);
+
+				using (var tr = db.BeginTransaction())
+				{
+					tr.Set("0", await strs.InternAsync(tr, "testing 123456789"));
+					tr.Set("1", await strs.InternAsync(tr, "dog"));
+					tr.Set("2", await strs.InternAsync(tr, "testing 123456789"));
+					tr.Set("3", await strs.InternAsync(tr, "cat"));
+					tr.Set("4", await strs.InternAsync(tr, "cat"));
+
+					tr.Set("9", "last");
+					tr.Set(":", "guard");
+
+					await tr.CommitAsync();
+				}
+
+				using (var tr = db.BeginTransaction())
+				{
+
+					Debug.WriteLine("GetRange('0'..'9') ....");
+		
+					var results = await tr.GetRangeAsync(
+						FdbKeySelector.FirstGreaterOrEqual(FdbKey.Ascii("0")),
+						FdbKeySelector.FirstGreaterThan(FdbKey.Ascii("9"))
+					);
+
+					Debug.WriteLine("Found " + results.Page.Length + " results");
+					foreach (var kvp in results.Page)
+					{
+						Debug.WriteLine(FdbKey.Dump(kvp.Key) + " : " + FdbKey.Dump(kvp.Value));
+					}
+
+					Debug.WriteLine("GetRange((BigStrings,*)) ....");
+
+					results = await tr.GetRangeAsync(FdbTuple.Create("BigStrings"));
+
+					Debug.WriteLine("Found " + results.Page.Length + " results");
+					foreach (var kvp in results.Page)
+					{
+						Debug.WriteLine(FdbKey.Dump(kvp.Key) + " : " + FdbKey.Dump(kvp.Value));
+					}
+
+					//var r = await tr.GetKeyAsync(FdbKeySelector.FirstGreaterOrEqual(FdbKey.Ascii("0")));
+					//Debug.WriteLine("First_Greater_Or_Equal('0') => " + DumpStr(r));
+					//r = await tr.GetKeyAsync(FdbKeySelector.FirstGreaterThan(FdbKey.Ascii("0")));
+					//Debug.WriteLine("First_Greater_Than('0') => " + DumpStr(r));
+
+					//r = await tr.GetKeyAsync(FdbKeySelector.LastLessOrEqual(FdbKey.Ascii("4")));
+					//Debug.WriteLine("Last_Less_Or_Equal('4') => " + DumpStr(r));
+					//r = await tr.GetKeyAsync(FdbKeySelector.LastLessThan(FdbKey.Ascii("4")));
+					//Debug.WriteLine("Last_Less_Than('4') => " + DumpStr(r));
+
+					//r = await tr.GetKeyAsync(FdbKeySelector.LastLessOrEqual(FdbKey.Ascii("9")));
+					//Debug.WriteLine("Last_Less_Or_Equal('9') => " + DumpStr(r));
+					//r = await tr.GetKeyAsync(FdbKeySelector.LastLessThan(FdbKey.Ascii("9")));
+					//Debug.WriteLine("Last_Less_Than('9') => " + DumpStr(r));
+
+					//r = await tr.GetKeyAsync(FdbKeySelector.LastLessThan(FdbKey.Ascii("0")));
+					//Debug.WriteLine("Last_Less_Than('0') => " + DumpStr(r));
+					//r = await tr.GetKeyAsync(FdbKeySelector.FirstGreaterThan(FdbKey.Ascii("9")));
+					//Debug.WriteLine("First_Greater_Than('9') => " + DumpStr(r));
+
+					//r = await tr.GetKeyAsync(FdbKeySelector.LastLessThan(FdbKey.Ascii("\0")));
+					//Debug.WriteLine("Last_Less_Than(NUL) => " + DumpStr(r));
+
+				}
+			}
+		}
+
+		private static string DumpHex(ArraySegment<byte> seg)
+		{
+			var sb = new StringBuilder();
+			int n = seg.Count;
+			int i = seg.Offset;
+			while (n-- > 0)
+			{
+				sb.Append(seg.Array[i++].ToString("x2"));
+			}
+			return sb.ToString();
+		}
+
+		[Test]
+		public void Test_Connecting_To_Cluster_With_Cancelled_Token_Should_Fail()
+		{
+			using (var cts = new CancellationTokenSource())
+			{
+				cts.Cancel();
+
+				Assert.Throws<OperationCanceledException>(() => Fdb.OpenLocalClusterAsync(cts.Token).GetAwaiter().GetResult());
+			}
+		}
+
+	}
+}
