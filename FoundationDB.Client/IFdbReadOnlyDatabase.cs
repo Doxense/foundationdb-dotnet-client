d4073641eb605bc77d1c3fb222c12804a122e187
Christophe Chevalier <chevalier@doxense.com>
2013-09-30 15:42:08 +0200

    Added IFdbDatabase and IFdbReadOnlyDatabase interfaces
    - contains the BeginTransaction(..) and BeginReadOnlyTransaction(..) methods

---
+++ FoundationDB.Client/IFdbReadOnlyDatabase.cs
@@ -0,0 +1,58 @@
+﻿#region BSD Licence
+/* Copyright (c) 2013, Doxense SARL
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of Doxense nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#endregion
+
+namespace FoundationDB.Client
+{
+	using System;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>FoundationDB database instance that supports read operations.</summary>
+	public interface IFdbReadOnlyDatabase : IFdbReadOnlyTransactional
+	{
+		/// <summary>Name of the database</summary>
+		string Name { get; }
+
+		/// <summary>Returns a cancellation token that is linked with the lifetime of this database instance</summary>
+		/// <remarks>The token will be cancelled if the database instance is disposed</remarks>
+		CancellationToken Token { get; }
+
+		/// <summary>Start a new read-only transaction on this database</summary>
+		/// <param name="ct">Optional cancellation token that can abort all pending async operations started by this transaction.</param>
+		/// <returns>New transaction instance that can read from the database.</returns>
+		/// <remarks>You MUST call Dispose() on the transaction when you are done with it. You SHOULD wrap it in a 'using' statement to ensure that it is disposed in all cases.</remarks>
+		/// <example>
+		/// using(var tr = db.BeginReadOnlyTransaction(CancellationToken.None))
+		/// {
+		///		var result = await tr.Get(Slice.FromString("Hello"));
+		///		var items = await tr.GetRange(FdbKeyRange.StartsWith(Slice.FromString("ABC"))).ToListAsync();
+		/// }</example>
+		IFdbReadOnlyTransaction BeginReadOnlyTransaction(CancellationToken ct = default(CancellationToken));
+	}
+
+}
