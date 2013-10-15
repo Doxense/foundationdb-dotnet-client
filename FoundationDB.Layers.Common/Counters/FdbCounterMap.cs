c7bfa66a718334593040b35153bf985bedbd170c
Christophe Chevalier <chevalier@doxense.com>
2013-10-15 14:29:31 +0200

    Common Layers: very simple counter map layer that helps maintaining a set of counters in a more complex layer
    - Each counter is referenced by a key under a common subspace
    - Each counter is a stored in a fixed-size 64 bit signed value, in little-endian.
    - Increment/Decrement/Add/Substract are atomic (don't return the value)
    - ReadAsync to read the value, and may conflict
    - XYZThenReadAsync = return the new value of the counter, after the update
    - ReadThenXYZAsync = return the initial value of the counter, before the update

---
+++ FoundationDB.Layers.Common/Counters/FdbCounterMap.cs
@@ -0,0 +1,178 @@
+﻿#region BSD Licence
+/* Copyright (c) 2013, Doxense SARL
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of Doxense nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#endregion
+
+namespace FoundationDB.Layers.Counters
+{
+	using FoundationDB.Client;
+	using System;
+	using System.Threading.Tasks;
+
+	public sealed class FdbCounterMap
+	{
+
+		private static readonly Slice PlusOne = Slice.FromFixed64(1);
+		private static readonly Slice MinusOne = Slice.FromFixed64(-1);
+
+		public FdbCounterMap(FdbSubspace subspace)
+		{
+			if (subspace == null) throw new ArgumentNullException("subspace");
+
+			this.Subspace = subspace;
+		}
+
+		/// <summary>Subspace used as a prefix for all items in this counter list</summary>
+		public FdbSubspace Subspace { get; private set; }
+
+		private Slice GetCounterKey(Slice counterKey)
+		{
+			if (counterKey.IsNull) throw new ArgumentNullException("counterKey");
+			return this.Subspace.Concat(counterKey);
+		}
+
+		/// <summary>Add a value to a counter in one atomic operation</summary>
+		/// <param name="transaction"></param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <param name="value">Value that will be added</param>
+		/// <remarks>This operation will not cause the current transaction to conflict. It may create conflicts for transactions that would read the value of the counter.</remarks>
+		public void Add(IFdbTransaction transaction, Slice counterKey, long value)
+		{
+			if (transaction == null) throw new ArgumentNullException("transaction");
+
+			//REVIEW: we could no-op if value == 0 but this may change conflict behaviour for other transactions...
+			Slice param = value == 1 ? PlusOne : value == -1 ? MinusOne : Slice.FromFixed64(value);
+			transaction.AtomicAdd(GetCounterKey(counterKey), param);
+		}
+
+		/// <summary>Subtract a value from a counter in one atomic operation</summary>
+		/// <param name="transaction">Transaction to use for the operation</param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <param name="value">Value that will be substracted. If the value is zero</param>
+		/// <remarks>This operation will not cause the current transaction to conflict. It may create conflicts for transactions that would read the value of the counter.</remarks>
+		public void Subtract(IFdbTransaction transaction, Slice counterKey, long value)
+		{
+			Add(transaction, counterKey, -value);
+		}
+
+		/// <summary>Increment the value of a counter in one atomic operation</summary>
+		/// <param name="transaction">Transaction to use for the operation</param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <remarks>This operation will not cause the current transaction to conflict. It may create conflicts for transactions that would read the value of the counter.</remarks>
+		public void Increment(IFdbTransaction transaction, Slice counterKey)
+		{
+			Add(transaction, counterKey, 1);
+		}
+
+		/// <summary>Decrement the value of a counter in one atomic operation</summary>
+		/// <param name="transaction">Transaction to use for the operation</param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <remarks>This operation will not cause the current transaction to conflict. It may create conflicts for transactions that would read the value of the counter.</remarks>
+		public void Decrement(IFdbTransaction transaction, Slice counterKey)
+		{
+			Add(transaction, counterKey, -1);
+		}
+
+		/// <summary>Read the value of a counter</summary>
+		/// <param name="transaction">Transaction to use for the operation</param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <returns></returns>
+		public async Task<long?> ReadAsync(IFdbReadOnlyTransaction transaction, Slice counterKey)
+		{
+			var data = await transaction.GetAsync(GetCounterKey(counterKey)).ConfigureAwait(false);
+			if (data.IsNullOrEmpty) return default(long?);
+			return data.ToInt64();
+		}
+
+		/// <summary>Adds a value to a counter, and return its new value.</summary>
+		/// <param name="transaction">Transaction to use for the operation</param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <returns>New value of the counter. Returns <paramref name="value"/> if the counter did not exist previously.</returns>
+		/// <remarks>This method WILL conflict with other transactions!</remarks>
+		public async Task<long> AddThenReadAsync(IFdbTransaction transaction, Slice counterKey, long value)
+		{
+			if (transaction == null) throw new ArgumentNullException("transaction");
+
+			var key = GetCounterKey(counterKey);
+			var res = await transaction.GetAsync(key).ConfigureAwait(false);
+
+			if (!res.IsNullOrEmpty) value += res.ToInt64();
+			transaction.Set(key, Slice.FromFixed64(value));
+
+			return value;
+		}
+
+		public Task<long> SubtractThenReadAsync(IFdbTransaction transaction, Slice counterKey, long value)
+		{
+			return AddThenReadAsync(transaction, counterKey, -value);
+		}
+
+		public Task<long> IncrementThenReadAsync(IFdbTransaction transaction, Slice counterKey)
+		{
+			return AddThenReadAsync(transaction, counterKey, 1);
+		}
+
+		public Task<long> DecrementThenReadAsync(IFdbTransaction transaction, Slice counterKey)
+		{
+			return AddThenReadAsync(transaction, counterKey, -1);
+		}
+
+		/// <summary>Adds a value to a counter, but return its previous value.</summary>
+		/// <param name="transaction">Transaction to use for the operation</param>
+		/// <param name="counterKey">Key of the counter, relative to the list's subspace</param>
+		/// <returns>Previous value of the counter. Returns 0 if the counter did not exist previously.</returns>
+		/// <remarks>This method WILL conflict with other transactions!</remarks>
+		public async Task<long> ReadThenAddAsync(IFdbTransaction transaction, Slice counterKey, long value)
+		{
+			if (transaction == null) throw new ArgumentNullException("transaction");
+
+			var key = GetCounterKey(counterKey);
+			var res = await transaction.GetAsync(key).ConfigureAwait(false);
+
+			long previous = res.IsNullOrEmpty ? 0 : res.ToInt64();
+			transaction.Set(key, Slice.FromFixed64(value + previous));
+
+			return previous;
+		}
+
+		public Task<long> ReadThenSubtractAsync(IFdbTransaction transaction, Slice counterKey, long value)
+		{
+			return ReadThenAddAsync(transaction, counterKey, -value);
+		}
+
+		public Task<long> ReadThenIncrementAsync(IFdbTransaction transaction, Slice counterKey)
+		{
+			return ReadThenAddAsync(transaction, counterKey, 1);
+		}
+
+		public Task<long> ReadThenDecrementAsync(IFdbTransaction transaction, Slice counterKey)
+		{
+			return ReadThenAddAsync(transaction, counterKey, -1);
+		}
+
+	}
+
+}
---
+++ FoundationDB.Layers.Common/Counters/FdbCounterMap`1.cs
@@ -0,0 +1,138 @@
+﻿#region BSD Licence
+/* Copyright (c) 2013, Doxense SARL
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of Doxense nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#endregion
+
+namespace FoundationDB.Layers.Counters
+{
+	using FoundationDB.Client;
+	using FoundationDB.Layers.Tuples;
+	using System;
+	using System.Threading.Tasks;
+
+	/// <summary>Container for multiple 64-bit signed counters, indexed by a key of type <typeparamref name="T"/></summary>
+	/// <typeparam name="T">Type of the key for each counter</typeparam>
+	public class FdbCounterMap<T>
+	{
+		// This is just a wrapper around the non-generic FDbCounterList, and just adds encoding/decoding semantics.
+		// By default we use the Tuple layer to encode/decode the keys, but implementors just need to derive this class,
+		// and override the EncodeKey/DecodeKey methods to change the serialization to something else
+
+		/// <summary>Queue that is used for storage</summary>
+		internal FdbCounterMap Map { get; private set; }
+
+		/// <summary>Subspace used as a prefix for all items in this counter list</summary>
+		public FdbSubspace Subspace { get { return this.Map.Subspace; } }
+
+		public FdbCounterMap(FdbSubspace subspace)
+		{
+			this.Map = new FdbCounterMap(subspace);
+		}
+
+		/// <summary>Encode a <typeparamref name="T"/> into a Slice</summary>
+		/// <param name="value">Key that will be stored in the counter list</param>
+		protected virtual Slice EncodeKey(T value)
+		{
+			return FdbTuple.Pack(value);
+		}
+
+		/// <summary>Decode a Slice back into a <typeparamref name="T"/></summary>
+		/// <param name="packed">Packed version that was generated via a previous call to <see cref="EncodeKey"/></param>
+		/// <returns>Decoded key</returns>
+		protected virtual T DecodeKey(Slice packed)
+		{
+			if (packed.IsNullOrEmpty) return default(T);
+
+			return FdbTuple.UnpackSingle<T>(packed);
+		}
+
+		public void Add(IFdbTransaction transaction, T counterKey, long value)
+		{
+			this.Map.Add(transaction, EncodeKey(counterKey), value);
+		}
+
+		public void Subtract(IFdbTransaction transaction, T counterKey, long value)
+		{
+			this.Map.Add(transaction, EncodeKey(counterKey), -value);
+		}
+
+		public void Increment(IFdbTransaction transaction, T counterKey)
+		{
+			this.Map.Add(transaction, EncodeKey(counterKey), 1);
+		}
+
+		public void Decrement(IFdbTransaction transaction, T counterKey)
+		{
+			this.Map.Add(transaction, EncodeKey(counterKey), -1);
+		}
+
+		public Task<long?> ReadAsync(IFdbReadOnlyTransaction transaction, T counterKey)
+		{
+			return this.Map.ReadAsync(transaction, EncodeKey(counterKey));
+		}
+
+		public Task<long> AddThenReadAsync(IFdbTransaction transaction, T counterKey, long value)
+		{
+			return this.Map.AddThenReadAsync(transaction, EncodeKey(counterKey), value);
+		}
+
+		public Task<long> SubtractThenReadAsync(IFdbTransaction transaction, T counterKey, long value)
+		{
+			return this.Map.AddThenReadAsync(transaction, EncodeKey(counterKey), -value);
+		}
+
+		public Task<long> IncrementThenReadAsync(IFdbTransaction transaction, T counterKey)
+		{
+			return this.Map.AddThenReadAsync(transaction, EncodeKey(counterKey), 1);
+		}
+
+		public Task<long> DecrementThenReadAsync(IFdbTransaction transaction, T counterKey)
+		{
+			return this.Map.AddThenReadAsync(transaction, EncodeKey(counterKey), -1);
+		}
+
+		public Task<long> ReadThenAddAsync(IFdbTransaction transaction, T counterKey, long value)
+		{
+			return this.Map.ReadThenAddAsync(transaction, EncodeKey(counterKey), value);
+		}
+
+		public Task<long> ReadThenSubtractAsync(IFdbTransaction transaction, T counterKey, long value)
+		{
+			return this.Map.ReadThenAddAsync(transaction, EncodeKey(counterKey), -value);
+		}
+
+		public Task<long> ReadThenIncrementAsync(IFdbTransaction transaction, T counterKey)
+		{
+			return this.Map.ReadThenAddAsync(transaction, EncodeKey(counterKey), 1);
+		}
+
+		public Task<long> ReadThenDecrementAsync(IFdbTransaction transaction, T counterKey)
+		{
+			return this.Map.ReadThenAddAsync(transaction, EncodeKey(counterKey), -1);
+		}
+
+	}
+}
