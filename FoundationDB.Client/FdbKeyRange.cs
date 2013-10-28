69227b21ef85aca09548417101a03e2465417bde
Christophe Chevalier <chevalier@doxense.com>
2013-10-28 17:01:06 +0100

    Fixed FdbKeyRange.ToString()

--- FoundationDB.Client/FdbKeyRange.cs
+++ FoundationDB.Client/FdbKeyRange.cs
@@ -125,7 +125,7 @@ namespace FoundationDB.Client
 		/// <summary>Returns a printable version of the range</summary>
 		public override string ToString()
 		{
-			return "{\"" + this.Begin.ToString() + "\", \"" + this.End.ToString() + "}";
+			return "{" + FdbKey.Dump(this.Begin) + ", " + FdbKey.Dump(this.End) + "}";
 		}
 	
 	}
