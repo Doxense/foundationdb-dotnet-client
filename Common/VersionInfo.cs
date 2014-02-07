5c7d4f8e0729adbbb43222781a5c743c8b279fa8
Christophe Chevalier <chevalier@doxense.com>
2014-02-07 09:40:13 +0100

    Bumped assembly and nuget package version to 0.200-pre

--- Common/VersionInfo.cs
+++ Common/VersionInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.100.0.0")] //note: Only change this when doing breaking API changes!
-[assembly: AssemblyFileVersion("0.100.0.0")] //note: change this to "x.y.z.build_number" in a build step of your favorite C.I. build server
-[assembly: AssemblyInformationalVersion("0.100.0-pre")]  //note: this is the version seen by NuGet, add "-alpha"/"-beta"/"-rc1" at the end to create pre-release packages
+[assembly: AssemblyVersion("0.200.0.0")] //note: Only change this when doing breaking API changes!
+[assembly: AssemblyFileVersion("0.200.0.0")] //note: change this to "x.y.z.build_number" in a build step of your favorite C.I. build server
+[assembly: AssemblyInformationalVersion("0.200.0-pre")]  //note: this is the version seen by NuGet, add "-alpha"/"-beta"/"-rc1" at the end to create pre-release packages
