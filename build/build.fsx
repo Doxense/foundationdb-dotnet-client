5c8bc9e01c6f2c1347ba82799fa67ff57a4d82b9
Martin Schinz <martin.schinz@pebblecode.com>
2014-04-22 09:21:11 +0100

    add nuget package to build

--- build/build.fsx
+++ build/build.fsx
@@ -5,8 +5,10 @@ open Fake
 
 RestorePackages()
 
-    // Properties
+// Properties
 let buildDir = "./build/output/"
+let nugetOutDir = buildDir + "_packages/"
+let version = "0.205.0-pre"
 
 let BuildProperties =
     [
@@ -43,6 +45,35 @@ Target "Test" (fun _ ->
         fun p -> { p with DisableShadowCopy = true
                           OutputFile = buildDir + "TestResults.xml"
                           ExcludeCategory = "LongRunning,LocalCluster" }))
+
+let replaceVersionInNuspec nuspecFileName version =
+    let re = @"(?<start>\<version\>|""FoundationDB.Client""\s?version="")[^""<>]+(?<end>\<\/version\>|"")"
+    let nuspecContents = ReadFileAsString nuspecFileName
+    let replacedContents = regex_replace re (sprintf "${start}%s${end}" version) nuspecContents
+    WriteStringToFile false nuspecFileName replacedContents
+    
+
+Target "Release" (fun _ ->
+    let projects = [ "FoundationDb.Client"; "FoundationDb.Layers.Common" ]
+    CreateDir nugetOutDir
+    projects
+    |> List.iter (
+        fun name ->
+            let nuspec = sprintf @"build\%s.nuspec" name
+            replaceVersionInNuspec nuspec version
+            let binariesDir = sprintf "%s/%s/" buildDir name
+            NuGetPack (
+                fun p ->
+                    { p with WorkingDir = binariesDir
+                             OutputPath = binariesDir
+                             Version = version}) nuspec
+
+            let targetLoc = (buildDir + (sprintf "%s/%s.%s.nupkg" name name version))
+            trace targetLoc
+
+        (*MoveFile nugetOutDir (buildDir + (sprintf "%s/%s/.%s.nupkg" name name version))*)
+    )
+)
 
 Target "Default" (fun _ -> trace "Starting build")
 
