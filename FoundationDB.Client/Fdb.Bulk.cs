2f69c1a173af8d95c4f288e0d4d53ef5efd24fea
Christophe Chevalier <chevalier@doxense.com>
2014-04-24 23:05:48 +0200

    Bulk: added a Fdb.Bulk.InsertAsync(...) method for processing and writing sequence of items that may exceed the 10MB transaction limit (Work In Progress!)
    - Keep items in a pending batch, and tries to commit once the total transaction size size exceeds a threshold (or number of items, or time limit)
    - If a batch fails to commit and is retryable, it is split in two halves that are retried (recursively until it commits)

--- FoundationDB.Client/Fdb.Bulk.cs
+++ FoundationDB.Client/Fdb.Bulk.cs
@@ -1,5 +1,5 @@
 ﻿#region BSD Licence
-/* Copyright (c) 2013, Doxense SARL
+/* Copyright (c) 2013-2014, Doxense SAS
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -32,6 +32,7 @@ namespace FoundationDB.Client
 	using System;
 	using System.Collections.Generic;
 	using System.Diagnostics;
+	using System.Linq;
 	using System.Threading;
 	using System.Threading.Tasks;
 
@@ -43,11 +44,24 @@ namespace FoundationDB.Client
 
 			#region Writing...
 
-			/// <summary>Insert a potentially large sequence of key/value pairs into the database, by using as many transactions as necessary, and automatically scaling the size of each batch</summary>
+			/// <summary>Writes a potentially large sequence of key/value pairs into the database, by using as many transactions as necessary, and automatically scaling the size of each batch.</summary>
+			/// <param name="db">Database used for the operation</param>
+			/// <param name="data">Sequence of key/value pairs</param>
+			/// <param name="cancellationToken">Cancellation Token</param>
+			/// <returns>Total number of values inserted in the database</returns>
+			/// <remarks>In case of a non-retryable error, some of the keys may remain in the database. Other transactions running at the same time may observe only a fraction of the keys until the operation completes.</remarks>
+			public static Task<long> WriteAsync(IFdbDatabase db, IEnumerable<KeyValuePair<Slice, Slice>> data, CancellationToken cancellationToken)
+			{
+				return WriteAsync(db, data, null, cancellationToken);
+			}
+
+			/// <summary>Writes a potentially large sequence of key/value pairs into the database, by using as many transactions as necessary, and automatically scaling the size of each batch.</summary>
+			/// <param name="db">Database used for the operation</param>
 			/// <param name="data">Sequence of key/value pairs</param>
 			/// <param name="progress">Notify of the progress on this instance (or null)</param>
 			/// <param name="cancellationToken">Cancellation Token</param>
 			/// <returns>Total number of values inserted in the database</returns>
+			/// <remarks>In case of a non-retryable error, some of the keys may remain in the database. Other transactions running at the same time may observe only a fraction of the keys until the operation completes.</remarks>
 			public static async Task<long> WriteAsync(IFdbDatabase db, IEnumerable<KeyValuePair<Slice, Slice>> data, IProgress<long> progress, CancellationToken cancellationToken)
 			{
 				if (db == null) throw new ArgumentNullException("db");
@@ -59,7 +73,7 @@ namespace FoundationDB.Client
 				// Each transaction should try to never exceed ~1MB of size
 
 				int maxBatchCount = 2 * 1000;
-				int maxBatchSize = 1 * 1000 * 1000;
+				int maxBatchSize = 1 * 1000 * 1000; //REVIEW: 100KB may be better, and we could also try with multiple transactions in // ?
 
 				var chunk = new List<KeyValuePair<Slice, Slice>>();
 
@@ -109,6 +123,234 @@ namespace FoundationDB.Client
 				return items;
 			}
 
+			/// <summary>Inserts a potentially large sequence of items into the database, by using as many transactions as necessary, and automatically retrying if needed.</summary>
+			/// <typeparam name="T">Type of the items in the <paramref name="source"/> sequence</typeparam>
+			/// <param name="db">Database used for the operation</param>
+			/// <param name="source">Sequence of items to be processed</param>
+			/// <param name="handler">Lambda called at least once for each item in the source. The method may not have any side effect outside of the passed transaction.</param>
+			/// <param name="cancellationToken">Token used to cancel the operation</param>
+			/// <returns>Number of items that have been inserted</returns>
+			/// <remarks>In case of a non-retryable error, some of the items may remain in the database. Other transactions running at the same time may observe only a fraction of the items until the operation completes.</remarks>
+			public static Task<long> InsertAsync<T>(IFdbDatabase db, IEnumerable<T> source, Action<T, IFdbTransaction> handler, CancellationToken cancellationToken)
+			{
+				if (db == null) throw new ArgumentNullException("db");
+				if (source == null) throw new ArgumentNullException("source");
+				if (handler == null) throw new ArgumentNullException("handler");
+
+				cancellationToken.ThrowIfCancellationRequested();
+
+				return RunWriteOperationAsync<T>(
+					db,
+					source,
+					handler,
+					1000, //TODO: configure?
+					cancellationToken
+				);
+			}
+
+			/// <summary>Inserts a potentially large sequence of items into the database, by using as many transactions as necessary, and automatically retrying if needed.</summary>
+			/// <typeparam name="T">Type of the items in the <paramref name="source"/> sequence</typeparam>
+			/// <param name="db">Database used for the operation</param>
+			/// <param name="source">Sequence of items to be processed</param>
+			/// <param name="handler">Lambda called at least once for each item in the source. The method may not have any side effect outside of the passed transaction.</param>
+			/// <param name="cancellationToken">Token used to cancel the operation</param>
+			/// <returns>Number of items that have been inserted</returns>
+			/// <remarks>In case of a non-retryable error, some of the items may remain in the database. Other transactions running at the same time may observe only a fraction of the items until the operation completes.</remarks>
+			public static Task<long> InsertAsync<T>(IFdbDatabase db, IEnumerable<T> source, Func<T, IFdbTransaction, Task> handler, CancellationToken cancellationToken)
+			{
+				if (db == null) throw new ArgumentNullException("db");
+				if (source == null) throw new ArgumentNullException("source");
+				if (handler == null) throw new ArgumentNullException("handler");
+
+				cancellationToken.ThrowIfCancellationRequested();
+
+				return RunWriteOperationAsync<T>(
+					db,
+					source,
+					handler,
+					1000, //TODO: configure?
+					cancellationToken
+				);
+			}
+
+			/// <summary>Runs a long duration bulk write</summary>
+			internal static async Task<long> RunWriteOperationAsync<TSource>(
+				IFdbDatabase db,
+				IEnumerable<TSource> source,
+				Delegate body,
+				int initialBatchSize,
+				CancellationToken cancellationToken
+			)
+			{
+				Contract.Requires(db != null && source != null && initialBatchSize > 0 && body != null);
+				
+				var bodyAsync = body as Func<TSource, IFdbTransaction, Task>;
+				var bodyBlocking = body as Action<TSource, IFdbTransaction>;
+				if (bodyAsync == null && bodyBlocking == null)
+				{
+					throw new ArgumentException(String.Format("Unsupported delegate type {0} for body", body.GetType().FullName), "body");
+				}
+
+				int sizeThreshold = 100 * 1000; //TODO: more? less? dynamic?
+				int batchSize = initialBatchSize;
+				var batch = new List<TSource>(batchSize);
+				long itemCount = 0;
+
+				using (var trans = db.BeginTransaction(cancellationToken))
+				{
+					var timer = Stopwatch.StartNew();
+
+					Func<Task> commit = async () =>
+					{
+						FdbException error = null;
+
+						// if transaction Size is bigger than Fdb.MaxTransactionSize (10MB) then commit will fail, but we will retry with a smaller batch anyway
+
+						try
+						{
+							await trans.CommitAsync().ConfigureAwait(false);
+						}
+						catch (FdbException e)
+						{ // the batch failed to commit :(
+							error = e;
+							//TODO: C# 6.0 will support awaits in catch blocks!
+						}
+
+						if (error != null)
+						{ // we failed to commit this batch, we need to retry...
+
+							if (error.Code == FdbError.TransactionTooLarge)
+							{
+								if (batch.Count == 1) throw new InvalidOperationException("Cannot insert one the item of the source collection because it exceeds the maximum size allowed per transaction");
+							}
+							else
+							{
+								await trans.OnErrorAsync(error.Code).ConfigureAwait(false);
+							}
+
+							int half = checked(batch.Count + 1) >> 1;
+							// retry the first half
+							await RetryChunk(trans, batch, 0, half, bodyAsync, bodyBlocking).ConfigureAwait(false);
+							// retry the second half
+							await RetryChunk(trans, batch, half, batch.Count - half, bodyAsync, bodyBlocking).ConfigureAwait(false);
+						}
+
+						// success!
+						batch.Clear();
+						trans.Reset();
+						timer.Reset();
+					};
+
+					foreach(var item in source)
+					{
+						if (cancellationToken.IsCancellationRequested) break;
+
+						// store it (in case we need to retry)
+						batch.Add(item);
+						++itemCount;
+
+						if (bodyAsync != null)
+						{
+							await bodyAsync(item, trans);
+						}
+						else if (bodyBlocking !=null)
+						{
+							bodyBlocking(item, trans);
+						}
+
+						// commit the batch if ..
+						if (trans.Size >= sizeThreshold			// transaction is startting to get big...
+						 || batch.Count >= batchSize			// too many items would need to be retried...
+						 || timer.Elapsed.TotalSeconds >= 4		// it's getting late...
+						)
+						{
+							await commit().ConfigureAwait(false);
+						}
+					}
+
+					cancellationToken.ThrowIfCancellationRequested();
+
+					// handle the last (or only) batch
+					if (batch.Count > 0)
+					{
+						await commit().ConfigureAwait(false);
+					}
+				}
+
+				return itemCount;
+			}
+
+			/// <summary>Retry commiting a segment of a batch, splitting it in sub-segments as needed</summary>
+			private static async Task RetryChunk<TSource>(IFdbTransaction trans, List<TSource> chunk, int offset, int count, Func<TSource, IFdbTransaction, Task> bodyAsync, Action<TSource, IFdbTransaction> bodyBlocking)
+			{
+				Contract.Requires(trans != null && chunk != null && offset >= 0 && count >= 0 && (bodyAsync != null || bodyBlocking != null));
+
+				// Steps:
+				// - reset transaction
+				// - replay the items in the batch segment
+				// - try to commit
+				// - it still fails, split segment into and retry each half
+
+				if (count <= 0) return;
+
+			localRetry:
+
+				trans.Cancellation.ThrowIfCancellationRequested();
+
+				trans.Reset();
+
+				if (bodyAsync != null)
+				{
+					for (int i = 0; i < count; i++)
+					{
+						await bodyAsync(chunk[offset + i], trans).ConfigureAwait(false);
+					}
+				}
+				else if (bodyBlocking != null)
+				{
+					for (int i = 0; i < count; i++)
+					{
+						bodyBlocking(chunk[offset + i], trans);
+					}
+				}
+
+				FdbException error;
+				try
+				{
+					await trans.CommitAsync().ConfigureAwait(false);
+					return;
+				}
+				catch (FdbException e)
+				{
+					error = e;
+					//TODO: update this for C# 6.0
+				}
+
+				// it failed again
+				if (error.Code == FdbError.TransactionTooLarge)
+				{
+					// retrying won't help if a single item is too big
+					if (count == 1) throw new InvalidOperationException("Cannot insert one the item of the source collection because it exceeds the maximum size allowed per transaction");
+				}
+				else
+				{
+					await trans.OnErrorAsync(error.Code).ConfigureAwait(false);
+
+				}
+
+				//TODO: for the moment we do a recursive call, which could potentially cause a stack overflow in addition to being ugly.
+				// => rewrite this to use a stack of work to do, without the need to recurse!
+
+				if (count == 1)
+				{ // protect against stackoverflow if we retry on a single item
+					goto localRetry;
+				}
+
+				int half = checked(count + 1) >> 1;
+				await RetryChunk(trans, chunk, offset, half, bodyAsync, bodyBlocking).ConfigureAwait(false);
+				await RetryChunk(trans, chunk, offset + half, count - half, bodyAsync, bodyBlocking).ConfigureAwait(false);
+			}
+
 			#endregion
 
 			#region Batching...
@@ -118,7 +360,7 @@ namespace FoundationDB.Client
 			private const int DefaultMinimumBatchSize = 4;
 			private const int DefaultMaximumBatchSize = 10000;
 
-
+			/// <summary>Context for a long running batched read operation</summary>
 			public sealed class BatchOperationContext
 			{
 				/// <summary>Transaction corresponding to the current generation</summary>
@@ -157,6 +399,7 @@ namespace FoundationDB.Client
 					this.Abort = true;
 				}
 
+				/// <summary>Gets or sets the abort flag</summary>
 				public bool Abort { get; set; }
 
 			}
@@ -187,7 +430,7 @@ namespace FoundationDB.Client
 				if (body == null) throw new ArgumentNullException("body");
 				if (localFinally == null) throw new ArgumentNullException("localFinally");
 
-				return RunBatchedOperationAsync<TSource, TLocal, object>(db, source, localInit, body, localFinally, DefaultInitialBatchSize, cancellationToken);
+				return RunBatchedReadOperationAsync<TSource, TLocal, object>(db, source, localInit, body, localFinally, DefaultInitialBatchSize, cancellationToken);
 			}
 
 			/// <summary>Execute a potentially long read-only operation on batches of elements from a source sequence, using as many transactions as necessary, and automatically scaling the size of each batch to maximize the throughput.</summary>
@@ -220,7 +463,7 @@ namespace FoundationDB.Client
 				// > either it's not async, then it could only Write/Clear, and in which case we need a writeable transaction ... ? (and who will commit and when ??)
 				// It could maybe make sense if the source was an IFdbAsyncEnumerable<T> because you could not use Parallel.ForEach(...) for that
 
-				return RunBatchedOperationAsync<TSource, TLocal, object>(db, source, localInit, body, localFinally, DefaultInitialBatchSize, cancellationToken);
+				return RunBatchedReadOperationAsync<TSource, TLocal, object>(db, source, localInit, body, localFinally, DefaultInitialBatchSize, cancellationToken);
 			}
 
 			/// <summary>Execute a potentially long read-only operation on batches of elements from a source sequence, using as many transactions as necessary, and automatically scaling the size of each batch to maximize the throughput.</summary>
@@ -240,7 +483,7 @@ namespace FoundationDB.Client
 				if (source == null) throw new ArgumentNullException("source");
 				if (body == null) throw new ArgumentNullException("body");
 
-				return RunBatchedOperationAsync<TSource, object, object>(db, source, null, body, null, DefaultInitialBatchSize, cancellationToken);
+				return RunBatchedReadOperationAsync<TSource, object, object>(db, source, null, body, null, DefaultInitialBatchSize, cancellationToken);
 			}
 
 			/// <summary>Execute a potentially long read-only operation on batches of elements from a source sequence, using as many transactions as necessary, and automatically scaling the size of each batch to maximize the throughput.</summary>
@@ -266,7 +509,7 @@ namespace FoundationDB.Client
 				// > either it's not async, then it could only Write/Clear, and in which case we need a writeable transaction ... ? (and who will commit and when ??)
 				// It could maybe make sense if the source was an IFdbAsyncEnumerable<T> because you could not use Parallel.ForEach(...) for that
 
-				return RunBatchedOperationAsync<TSource, object, object>(db, source, null, body, null, DefaultInitialBatchSize, cancellationToken);
+				return RunBatchedReadOperationAsync<TSource, object, object>(db, source, null, body, null, DefaultInitialBatchSize, cancellationToken);
 			}
 
 			#endregion
@@ -295,16 +538,18 @@ namespace FoundationDB.Client
 				if (body == null) throw new ArgumentNullException("body");
 
 				Func<TAggregate, TAggregate> identity = (x) => x;
-				return RunBatchedOperationAsync<TSource, TAggregate, TAggregate>(db, source, localInit, body, identity, DefaultInitialBatchSize, cancellationToken);
+				return RunBatchedReadOperationAsync<TSource, TAggregate, TAggregate>(db, source, localInit, body, identity, DefaultInitialBatchSize, cancellationToken);
 			}
 
 			/// <summary>Execute a potentially long aggregation on batches of elements from a source sequence, using as many transactions as necessary, and automatically scaling the size of each batch to maximize the throughput.</summary>
 			/// <typeparam name="TSource">Type of elements in the source sequence</typeparam>
 			/// <typeparam name="TAggregate">Type of the local immutable aggregate that is flowed accross all batch operations</typeparam>
+			/// <typeparam name="TResult">Type of the result of the operation</typeparam>
 			/// <param name="db">Source database</param>
 			/// <param name="source">Source sequence that will be split into batch. The size of each batch will scale up and down automatically depending on the speed of execution</param>
-			/// <param name="localInit">Lambda function that is called once, and returns the initial state that will be passed to the first batch</param>
+			/// <param name="init">Lambda function that is called once, and returns the initial state that will be passed to the first batch</param>
 			/// <param name="body">Retryable lambda function that receives a batch of elements from the source sequence, the current context, and the previous state. If the transaction expires while this lambda function is running, it will be automatically retried with a new transaction, and a smaller batch.</param>
+			/// <param name="transform">Lambda function called with the aggregate returned by the last batch, and which will compute the final result of the operation</param>
 			/// <param name="cancellationToken">Token used to cancel the operation</param>
 			/// <returns>Task that completes when all the elements of <paramref name="source"/> have been processed, a non-retryable error occurs, or <paramref name="cancellationToken"/> is triggered</returns>
 			public static Task<TResult> AggregateAsync<TSource, TAggregate, TResult>(
@@ -321,13 +566,13 @@ namespace FoundationDB.Client
 				if (body == null) throw new ArgumentNullException("body");
 				if (transform == null) throw new ArgumentNullException("transform");
 
-				return RunBatchedOperationAsync<TSource, TAggregate, TResult>(db, source, init, body, transform, DefaultInitialBatchSize, cancellationToken);
+				return RunBatchedReadOperationAsync<TSource, TAggregate, TResult>(db, source, init, body, transform, DefaultInitialBatchSize, cancellationToken);
 			}
 
 			#endregion
 
 			/// <summary>Runs a long duration bulk read</summary>
-			private static async Task<TResult> RunBatchedOperationAsync<TSource, TLocal, TResult>(
+			internal static async Task<TResult> RunBatchedReadOperationAsync<TSource, TLocal, TResult>(
 				IFdbDatabase db,
 				IEnumerable<TSource> source,
 				Func<TLocal> localInit,
