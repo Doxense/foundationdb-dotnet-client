958714c28bd7dc9b23d6957bfb3d74c2a5844e21
Christophe Chevalier <chevalier@doxense.com>
2014-06-20 15:48:56 +0200

    Added various Decode helpers on FdbRangeChunk
    - decode all the key and/or value slices of a result page into an array of typed results

--- FoundationDB.Client/FdbRangeChunk.cs
+++ FoundationDB.Client/FdbRangeChunk.cs
@@ -28,6 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace FoundationDB.Client
 {
+	using JetBrains.Annotations;
 	using System;
 	using System.Collections.Generic;
 	using System.Diagnostics;
@@ -86,6 +87,175 @@ namespace FoundationDB.Client
 			}
 		}
 
+		/// <summary>Decode the content of this chunk into an array of typed key/value pairs</summary>
+		/// <typeparam name="TKey">Type of the keys</typeparam>
+		/// <typeparam name="TValue">Type of the values</typeparam>
+		/// <param name="keyEncoder">Lambda that can decode the keys of this chunk</param>
+		/// <param name="valueEncoder">Lambda that can decode the values of this chunk</param>
+		/// <returns>Array of decoded key/value pairs, or an empty array if the chunk doesn't have any results</returns>
+		[NotNull]
+		public KeyValuePair<TKey, TValue>[] Decode<TKey, TValue>([NotNull] Func<Slice, TKey> keyHandler, [NotNull] Func<Slice, TValue> valueHandler)
+		{
+			if (keyHandler == null) throw new ArgumentNullException("keyHandler");
+			if (valueHandler == null) throw new ArgumentNullException("valueHandler");
+
+			var chunk = this.Chunk;
+			var results = new KeyValuePair<TKey, TValue>[chunk.Length];
+
+			for (int i = 0; i < chunk.Length; i++)
+			{
+				results[i] = new KeyValuePair<TKey, TValue>(
+					keyHandler(chunk[i].Key),
+					valueHandler(chunk[i].Value)
+				);
+			}
+
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed key/value pairs</summary>
+		/// <typeparam name="TKey">Type of the keys</typeparam>
+		/// <typeparam name="TValue">Type of the values</typeparam>
+		/// <param name="subspace">Subspace that is expected to contain all the keys.</param>
+		/// <param name="keyEncoder">Instance used to decode the keys of this chunk. The prefix of <paramref name="subspace"/> is removed from the key before calling the encoder.</param>
+		/// <param name="valueEncoder">Instance used to decode the values of this chunk</param>
+		/// <returns>Array of decoded key/value pairs, or an empty array if the chunk doesn't have any results</returns>
+		/// <exception cref="System.ArgumentException">If at least on key in the result is outside <paramref name="subspace"/>.</exception>
+		/// <exception cref="System.ArgumentNullException">If either <paramref name="subspace"/>, <paramref name="keyEncoder"/> or <paramref name="valueEncoder"/> is null.</exception>
+		[NotNull]
+		public KeyValuePair<TKey, TValue>[] Decode<TKey, TValue>([NotNull] FdbSubspace subspace, [NotNull] IKeyEncoder<TKey> keyEncoder, [NotNull] IValueEncoder<TValue> valueEncoder)
+		{
+			if (subspace == null) throw new ArgumentNullException("subspace");
+			if (keyEncoder == null) throw new ArgumentNullException("keyEncoder");
+			if (valueEncoder == null) throw new ArgumentNullException("valueEncoder");
+
+			var chunk = this.Chunk;
+			var results = new KeyValuePair<TKey, TValue>[chunk.Length];
+
+			for (int i = 0; i < chunk.Length; i++)
+			{
+				results[i] = new KeyValuePair<TKey, TValue>(
+					keyEncoder.DecodeKey(subspace.ExtractAndCheck(chunk[i].Key)),
+					valueEncoder.DecodeValue(chunk[i].Value)
+				);
+			}
+
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed key/value pairs</summary>
+		/// <typeparam name="TKey">Type of the keys</typeparam>
+		/// <typeparam name="TValue">Type of the values</typeparam>
+		/// <param name="keyEncoder">Instance used to decode the keys of this chunk</param>
+		/// <param name="valueEncoder">Instance used to decode the values of this chunk</param>
+		/// <returns>Array of decoded key/value pairs, or an empty array if the chunk doesn't have any results</returns>
+		/// <exception cref="System.ArgumentNullException">If either <paramref name="keyEncoder"/> or <paramref name="valueEncoder"/> is null.</exception>
+		[NotNull]
+		public KeyValuePair<TKey, TValue>[] Decode<TKey, TValue>([NotNull] IKeyEncoder<TKey> keyEncoder, [NotNull] IValueEncoder<TValue> valueEncoder)
+		{
+			if (keyEncoder == null) throw new ArgumentNullException("keyEncoder");
+			if (valueEncoder == null) throw new ArgumentNullException("valueEncoder");
+
+			var chunk = this.Chunk;
+			var results = new KeyValuePair<TKey, TValue>[chunk.Length];
+
+			for (int i = 0; i < chunk.Length; i++)
+			{
+				results[i] = new KeyValuePair<TKey, TValue>(
+					keyEncoder.DecodeKey(chunk[i].Key),
+					valueEncoder.DecodeValue(chunk[i].Value)
+				);
+			}
+
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed keys</summary>
+		/// <typeparam name="T">Type of the keys</typeparam>
+		/// <param name="keyEncoder">Instance used to decode the keys of this chunk</param>
+		/// <returns>Array of decoded keys, or an empty array if the chunk doesn't have any results</returns>
+		[NotNull]
+		public T[] DecodeKeys<T>([NotNull] Func<Slice, T> handler)
+		{
+			if (handler == null) throw new ArgumentNullException("handler");
+
+			var results = new T[this.Count];
+			for (int i = 0; i < results.Length; i++)
+			{
+				results[i] = handler(this.Chunk[i].Key);
+			}
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed keys</summary>
+		/// <typeparam name="T">Type of the keys</typeparam>
+		/// <param name="keyEncoder">Instance used to decode the keys of this chunk</param>
+		/// <returns>Array of decoded keys, or an empty array if the chunk doesn't have any results</returns>
+		[NotNull]
+		public T[] DecodeKeys<T>([NotNull] FdbSubspace subspace, [NotNull] IKeyEncoder<T> keyEncoder)
+		{
+			if (subspace == null) throw new ArgumentNullException("subspace");
+			if (keyEncoder == null) throw new ArgumentNullException("keyEncoder");
+
+			var results = new T[this.Count];
+			for(int i = 0; i< results.Length;i++)
+			{
+				results[i] = keyEncoder.DecodeKey(subspace.ExtractAndCheck(this.Chunk[i].Key));
+			}
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed keys</summary>
+		/// <typeparam name="T">Type of the keys</typeparam>
+		/// <param name="keyEncoder">Instance used to decode the keys of this chunk</param>
+		/// <returns>Array of decoded keys, or an empty array if the chunk doesn't have any results</returns>
+		[NotNull]
+		public T[] DecodeKeys<T>([NotNull] IKeyEncoder<T> keyEncoder)
+		{
+			if (keyEncoder == null) throw new ArgumentNullException("keyEncoder");
+
+			var results = new T[this.Count];
+			for (int i = 0; i < results.Length; i++)
+			{
+				results[i] = keyEncoder.DecodeKey(this.Chunk[i].Key);
+			}
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed values</summary>
+		/// <typeparam name="T">Type of the values</typeparam>
+		/// <param name="handler">Lambda that can decode the values of this chunk</param>
+		/// <returns>Array of decoded values, or an empty array if the chunk doesn't have any results</returns>
+		[NotNull]
+		public T[] DecodeValues<T>([NotNull] Func<Slice, T> handler)
+		{
+			if (handler == null) throw new ArgumentNullException("handler");
+
+			var results = new T[this.Count];
+			for (int i = 0; i < results.Length; i++)
+			{
+				results[i] = handler(this.Chunk[i].Value);
+			}
+			return results;
+		}
+
+		/// <summary>Decode the content of this chunk into an array of typed values</summary>
+		/// <typeparam name="T">Type of the values</typeparam>
+		/// <param name="keyEncoder">Instance used to decode the values of this chunk</param>
+		/// <returns>Array of decoded values, or an empty array if the chunk doesn't have any results</returns>
+		[NotNull]
+		public T[] DecodeValues<T>([NotNull] IValueEncoder<T> valueEncoder)
+		{
+			if (valueEncoder == null) throw new ArgumentNullException("valueEncoder");
+
+			var results = new T[this.Count];
+			for (int i = 0; i < results.Length; i++)
+			{
+				results[i] = valueEncoder.DecodeValue(this.Chunk[i].Value);
+			}
+			return results;
+		}
+	
 	}
 
 }
