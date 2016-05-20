ca12a90ba0cce7a0dbd00adddc123a4a966f3960
Grégoire Castre <castre@doxense.com>
2016-05-20 16:02:37 +0200

    Ajout du ItemCanBeNull sur ReadAsync

--- FoundationDB.Client/IFdbReadOnlyRetryable.cs
+++ FoundationDB.Client/IFdbReadOnlyRetryable.cs
@@ -79,6 +79,7 @@ namespace FoundationDB.Client
 		/// Since the handler can run more than once, and that there is no guarantee that the transaction commits once it returns, you MAY NOT mutate any global state (counters, cache, global dictionary) inside this lambda!
 		/// You must wait for the Task to complete successfully before updating the global state of the application.
 		/// </remarks>
+		[ItemCanBeNull]
 		Task<R> ReadAsync<R>([NotNull, InstantHandle] Func<IFdbReadOnlyTransaction, Task<R>> asyncHandler, CancellationToken cancellationToken);
 
 		//REVIEW: should we keep these ?
