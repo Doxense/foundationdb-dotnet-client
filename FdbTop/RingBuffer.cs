cd817d271e78453982bb0302c68256c045d73d93
Christophe Chevalier <chevalier@doxense.com>
2018-04-23 18:48:38 +0200

    Don't display the column guids in DEBUG + code cleanup

--- FdbTop/RingBuffer.cs
+++ FdbTop/RingBuffer.cs
@@ -1,15 +1,36 @@
-﻿using FoundationDB.Client;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿#region BSD Licence
+/* Copyright (c) 2013-2018, Doxense SAS
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of Doxense nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#endregion
 
 namespace FdbTop
 {
+	using System;
+	using System.Collections.Generic;
+
 	public class RingBuffer<T> : IReadOnlyCollection<T>
 	{
 		private readonly Queue<T> m_store;
@@ -17,21 +38,15 @@ namespace FdbTop
 
 		public RingBuffer(int capacity)
 		{
-			if (capacity < 0) throw new ArgumentOutOfRangeException("capacity");
+			if (capacity < 0) throw new ArgumentOutOfRangeException(nameof(capacity));
 
 			m_store = new Queue<T>(capacity);
 			m_size = capacity;
 		}
 
-		public int Count
-		{
-			get { return m_store.Count; }
-		}
+		public int Count => m_store.Count;
 
-		public int Capacity
-		{
-			get { return m_size; }
-		}
+		public int Capacity => m_size;
 
 		public void Clear()
 		{
@@ -40,7 +55,7 @@ namespace FdbTop
 
 		public void Resize(int newCapacity)
 		{
-			if (newCapacity < 0) throw new ArgumentOutOfRangeException("newCapacity");
+			if (newCapacity < 0) throw new ArgumentOutOfRangeException(nameof(newCapacity));
 
 			var store = m_store;
 			if (newCapacity < store.Count)
