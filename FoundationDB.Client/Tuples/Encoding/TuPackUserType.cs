6e41c2ecc9b92034ccf0e0e7efb0cbd20979c6a8
Christophe Chevalier <chevalier@doxense.com>
2018-05-31 23:20:10 +0200

    Mark TuPackUserType as part of the shared framework

--- FoundationDB.Client/Tuples/Encoding/TuPackUserType.cs
+++ FoundationDB.Client/Tuples/Encoding/TuPackUserType.cs
@@ -26,6 +26,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #endregion
 
+#if !USE_SHARED_FRAMEWORK
+
 namespace Doxense.Collections.Tuples.Encoding
 {
 	using System;
@@ -95,3 +97,5 @@ namespace Doxense.Collections.Tuples.Encoding
 		#endregion
 	}
 }
+
+#endif
