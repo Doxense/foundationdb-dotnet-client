0247f87dd7fef3c64f990fd0b61e99925acbdb64
Christophe Chevalier <chevalier@doxense.com>
2019-11-09 16:11:19 +0900

    Set default and max API level to 620

--- FoundationDB.Client/Fdb.cs
+++ FoundationDB.Client/Fdb.cs
@@ -66,7 +66,7 @@ namespace FoundationDB.Client
 		internal const int MaxSafeApiVersion = FdbNative.FDB_API_MAX_VERSION;
 
 		/// <summary>Default API version that will be selected, if the application does not specify otherwise.</summary>
-		internal const int DefaultApiVersion = 610; // v6.1.x
+		internal const int DefaultApiVersion = 620; // v6.2.x
 		//INVARIANT: MinSafeApiVersion <= DefaultApiVersion <= MaxSafeApiVersion
 
 		#endregion
