7b49bdad465bf85b1a572ac67f016e4c45e33acd
Christophe Chevalier <chevalier@doxense.com>
2019-11-21 14:55:47 +0100

    Fix build.sh script for .NET Core 3.0 on non-Windows platforms

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-dotnet build FoundationDB.Client/FoundationDB.Client.csproj -c Debug -f netstandard2.0 --version-suffix dev
-dotnet build FdbShell/FdbShell.csproj -c Debug -f netcoreapp2.2 --version-suffix dev
-dotnet build FdbTop/FdbTop.csproj -c Debug -f netcoreapp2.2 --version-suffix dev
+dotnet build FoundationDB.Client/FoundationDB.Client.csproj -c Debug -f netstandard2.1 --version-suffix dev
+dotnet build FoundationDB.DependencyInjection/FoundationDB.DependencyInjection.csproj -c Debug -f netstandard2.1 --version-suffix dev
+dotnet build FoundationDB.Layers.Common/FoundationDB.Layers.Common.csproj -c Debug -f netstandard2.1 --version-suffix dev
+dotnet build FoundationDB.Linq.Providers/FoundationDB.Linq.Providers.csproj -c Debug -f netstandard2.1 --version-suffix dev
+dotnet build FdbShell/FdbShell.csproj -c Debug -f netcoreapp3.0 --version-suffix dev
+dotnet build FdbTop/FdbTop.csproj -c Debug -f netcoreapp3.0 --version-suffix dev
