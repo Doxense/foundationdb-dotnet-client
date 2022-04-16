133e07e0473b08c513ee2ff1b403504d66d125b0
Christophe Chevalier <chevalier@doxense.com>
2022-04-16 18:20:11 +0200

    cleanup xml comments

--- FoundationDB.Client/FdbMutationType.cs
+++ FoundationDB.Client/FdbMutationType.cs
@@ -41,34 +41,34 @@ namespace FoundationDB.Client
 		//note: there is no entry for 1
 
 		/// <summary>Performs an addition of little-endian integers.
-		/// If the existing value in the database is not present or shorter than ``param``, it is first extended to the length of ``param`` with zero bytes.
-		/// If ``param`` is shorter than the existing value in the database, the existing value is truncated to match the length of ``param``.
+		/// If the existing value in the database is not present or shorter than <c>param</c>, it is first extended to the length of <c>param</c> with zero bytes.
+		/// If <c>param</c> is shorter than the existing value in the database, the existing value is truncated to match the length of <c>param</c>.
 		/// The integers to be added must be stored in a little-endian representation.
 		/// They can be signed in two's complement representation or unsigned.
-		/// You can add to an integer at a known offset in the value by prepending the appropriate number of zero bytes to ``param`` and padding with zero bytes to match the length of the value.
+		/// You can add to an integer at a known offset in the value by prepending the appropriate number of zero bytes to <c>param</c> and padding with zero bytes to match the length of the value.
 		/// However, this offset technique requires that you know the addition will not cause the integer field within the value to overflow.
 		/// </summary>
 		Add = 2,
 
-		/// <summary>Performs a bitwise ``and`` operation.
-		/// If the existing value in the database is not present or shorter than ``param``, it is first extended to the length of ``param`` with zero bytes.
-		/// If ``param`` is shorter than the existing value in the database, the existing value is truncated to match the length of ``param``.
+		/// <summary>Performs a bitwise <c>and</c> operation.
+		/// If the existing value in the database is not present or shorter than <c>param</c>, it is first extended to the length of <c>param</c> with zero bytes.
+		/// If <c>param</c> is shorter than the existing value in the database, the existing value is truncated to match the length of <c>param</c>.
 		/// </summary>
 		BitAnd = 6,
 
-		/// <summary>Performs a bitwise ``or`` operation.
-		/// If the existing value in the database is not present or shorter than ``param``, it is first extended to the length of ``param`` with zero bytes.
-		/// If ``param`` is shorter than the existing value in the database, the existing value is truncated to match the length of ``param``.
+		/// <summary>Performs a bitwise <c>or</c> operation.
+		/// If the existing value in the database is not present or shorter than <c>param</c>, it is first extended to the length of <c>param</c> with zero bytes.
+		/// If <c>param</c> is shorter than the existing value in the database, the existing value is truncated to match the length of <c>param</c>.
 		/// </summary>
 		BitOr = 7,
 
-		/// <summary>Performs a bitwise ``xor`` operation.
-		/// If the existing value in the database is not present or shorter than ``param``, it is first extended to the length of ``param`` with zero bytes.
-		/// If ``param`` is shorter than the existing value in the database, the existing value is truncated to match the length of ``param``.
+		/// <summary>Performs a bitwise <c>xor</c> operation.
+		/// If the existing value in the database is not present or shorter than <c>param</c>, it is first extended to the length of <c>param</c> with zero bytes.
+		/// If <c>param</c> is shorter than the existing value in the database, the existing value is truncated to match the length of <c>param</c>.
 		/// </summary>
 		BitXor = 8,
 
-		/// <summary>Appends ``param`` to the end of the existing value already in the database at the given key (or creates the key and sets the value to ``param`` if the key is empty).
+		/// <summary>Appends <c>param</c> to the end of the existing value already in the database at the given key (or creates the key and sets the value to <c>param</c> if the key is empty).
 		/// This will only append the value if the final concatenated value size is less than or equal to the maximum value size (i.e., if it fits).
 		/// WARNING: No error is surfaced back to the user if the final value is too large because the mutation will not be applied until after the transaction has been committed.
 		/// Therefore, it is only safe to use this mutation type if one can guarantee that one will keep the total value size under the maximum size.
@@ -76,23 +76,23 @@ namespace FoundationDB.Client
 		AppendIfFits = 9,
 
 		/// <summary>Performs a little-endian comparison of byte strings.
-		/// If the existing value in the database is not present or shorter than ``param``, it is first extended to the length of ``param`` with zero bytes.
-		/// If ``param`` is shorter than the existing value in the database, the existing value is truncated to match the length of ``param``.
+		/// If the existing value in the database is not present or shorter than <c>param</c>, it is first extended to the length of <c>param</c> with zero bytes.
+		/// If <c>param</c> is shorter than the existing value in the database, the existing value is truncated to match the length of <c>param</c>.
 		/// The larger of the two values is then stored in the database.
 		/// </summary>
 		Max = 12,
 
 		/// <summary>Performs a little-endian comparison of byte strings.
-		/// If the existing value in the database is not present or shorter than ``param``, it is first extended to the length of ``param`` with zero bytes.
-		/// If ``param`` is shorter than the existing value in the database, the existing value is truncated to match the length of ``param``.
+		/// If the existing value in the database is not present or shorter than <c>param</c>, it is first extended to the length of <c>param</c> with zero bytes.
+		/// If <c>param</c> is shorter than the existing value in the database, the existing value is truncated to match the length of <c>param</c>.
 		/// The smaller of the two values is then stored in the database.
 		/// </summary>
 		Min = 13,
 
-		/// <summary>Transforms ``key`` using a versionstamp for the transaction.
-		/// Sets the transformed key in the database to ``param``.
-		/// The key is transformed by removing the final four bytes from the key and reading those as a little-Endian 32-bit integer to get a position ``pos``.
-		/// The 10 bytes of the key from ``pos`` to ``pos + 10`` are replaced with the versionstamp of the transaction used.
+		/// <summary>Transforms <c>key</c> using a versionstamp for the transaction.
+		/// Sets the transformed key in the database to <c>param</c>.
+		/// The key is transformed by removing the final four bytes from the key and reading those as a little-Endian 32-bit integer to get a position <c>pos</c>.
+		/// The 10 bytes of the key from <c>pos</c> to <c>pos + 10</c> are replaced with the versionstamp of the transaction used.
 		/// The first byte of the key is position 0.
 		/// A versionstamp is a 10 byte, unique, monotonically (but not sequentially) increasing value for each committed transaction.
 		/// The first 8 bytes are the committed version of the database (serialized in big-Endian order).
@@ -101,10 +101,10 @@ namespace FoundationDB.Client
 		/// </summary>
 		VersionStampedKey = 14,
 
-		/// <summary>Transforms ``param`` using a versionstamp for the transaction.
-		/// Sets the ``key`` given to the transformed ``param``.
-		/// The parameter is transformed by removing the final four bytes from ``param`` and reading those as a little-Endian 32-bit integer to get a position ``pos``.
-		/// The 10 bytes of the parameter from ``pos`` to ``pos + 10`` are replaced with the versionstamp of the transaction used.
+		/// <summary>Transforms <c>param</c> using a versionstamp for the transaction.
+		/// Sets the <c>key</c> given to the transformed <c>param</c>.
+		/// The parameter is transformed by removing the final four bytes from <c>param</c> and reading those as a little-Endian 32-bit integer to get a position <c>pos</c>.
+		/// The 10 bytes of the parameter from <c>pos</c> to <c>pos + 10</c> are replaced with the versionstamp of the transaction used.
 		/// The first byte of the parameter is position 0.
 		/// A versionstamp is a 10 byte, unique, monotonically (but not sequentially) increasing value for each committed transaction.
 		/// The first 8 bytes are the committed version of the database (serialized in big-Endian order).
