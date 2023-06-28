c5f138d7451f8cd6636d98a823deb24fce6e1ac0
Christophe Chevalier <chevalier@doxense.com>
2023-06-28 10:57:57 +0200

    Update copyright header to 2023
    - fix all variants to be the same header

--- FoundationDB.Client/Native/Handles/TenantHandle.cs
+++ FoundationDB.Client/Native/Handles/TenantHandle.cs
@@ -1,5 +1,5 @@
 #region BSD License
-/* Copyright (c) 2013-2020, Doxense SAS
+/* Copyright (c) 2013-2023 Doxense SAS
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
