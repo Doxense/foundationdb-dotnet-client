3a89dbc6d1506f372f533e02ac98ef56cf17c857
Christophe Chevalier <chevalier@doxense.com>
2023-07-13 12:23:22 +0200

    Fix 'map' command in FdbShell when not in a partition

--- FdbShell/Commands/BasicCommands.cs
+++ FdbShell/Commands/BasicCommands.cs
@@ -731,12 +731,13 @@ namespace FdbShell
 
 			Program.StdOut(log, "Listing all shards...");
 
+			var dirLayer = path.Count > 0 ? folder.DirectoryLayer : db.DirectoryLayer;
 			// note: this may break in future versions of the DL! Maybe we need a custom API to get a flat list of all directories in a DL that span a specific range ?
-			var span = await db.ReadAsync(async tr => (await folder.DirectoryLayer.Content.Resolve(tr)).ToRange(), ct);
+			var span = await db.ReadAsync(async tr => (await dirLayer.Content.Resolve(tr))!.ToRange(), ct);
 
 			var shards = await Fdb.System.GetChunksAsync(db, span, ct);
 			int totalShards = shards.Count;
-			Program.StdOut(log, $"> Found {totalShards} shard(s) in partition {folder.DirectoryLayer.FullName}", ConsoleColor.Gray);
+			Program.StdOut(log, $"> Found {totalShards} shard(s) in partition {dirLayer.FullName}", ConsoleColor.Gray);
 
 			Program.StdOut(log, "Listing all directories...");
 			var map = new Dictionary<string, int>(StringComparer.Ordinal);
