acb607b2209e8e744abf5fcc6a4aae9a97329b82
Christophe Chevalier <chevalier@snowbank-labs.com>
2024-05-03 17:38:19 +0200

    core: remove unused methods from ExpcetionExtensions

--- Doxense.Core/Tools/ExceptionExtensions.cs
+++ Doxense.Core/Tools/ExceptionExtensions.cs
@@ -24,28 +24,11 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
-#nullable enable
-
 namespace Doxense.Tools
 {
-	using System.Reflection;
 
 	public static class ExceptionExtensions
 	{
-		private static readonly MethodInfo? s_preserveStackTrace = TryGetPreserveStackHandler();
-
-		private static MethodInfo? TryGetPreserveStackHandler()
-		{
-			// Existe sous NETFX et aussi sous .NET Core (en tout cas au moins sous .NET Core 3.0)
-			MethodInfo? handler = null;
-			try
-			{
-				handler = typeof(Exception).GetMethod("InternalPreserveStackTrace", BindingFlags.Instance | BindingFlags.NonPublic);
-			}
-			catch { }
-			Contract.Debug.Ensures(handler != null, "Exception.InternalPreserveStackTrace not found?");
-			return handler;
-		}
 
 		/// <summary>Détermine s'il s'agit d'une erreur fatale (qu'il faudrait bouncer)</summary>
 		/// <param name="self">Exception à tester</param>
@@ -53,7 +36,7 @@ namespace Doxense.Tools
 		[Pure]
 		public static bool IsFatalError([System.Diagnostics.CodeAnalysis.NotNullWhen(true)] this Exception? self)
 		{
-			return self is System.Threading.ThreadAbortException || self is OutOfMemoryException || self is StackOverflowException || (self is AggregateException && IsFatalError(self.InnerException));
+			return self is ThreadAbortException || self is OutOfMemoryException || self is StackOverflowException || (self is AggregateException && IsFatalError(self.InnerException));
 		}
 
 		/// <summary>Détermine s'il s'agit d'une erreur "interessante" pour le diagnostique de crash, et qui mérite d'écrire une stack-trace entière dans les logs</summary>
@@ -64,29 +47,6 @@ namespace Doxense.Tools
 			return self is NullReferenceException or ArgumentException or IndexOutOfRangeException or KeyNotFoundException || (self is AggregateException && IsLikelyBug(self.InnerException));
 		}
 
-		/// <summary>Préserve la stacktrace lorsqu'on crée une exception, qui sera re-throwé plus haut</summary>
-		/// <param name="self">Exception qui a été catchée</param>
-		/// <returns>La même exception, mais avec la StackTrace préservée</returns>
-		public static Exception PreserveStackTrace(this Exception self)
-		{
-			self = UnwrapIfAggregate(self);
-			if (s_preserveStackTrace != null) s_preserveStackTrace.Invoke(self, null);
-			return self;
-		}
-
-		/// <summary>Préserve la stacktrace lorsqu'on veut re-thrower une exception catchée</summary>
-		/// <param name="self">Exception qui a été catchée</param>
-		/// <returns>La même exception, mais avec la StackTrace préservée</returns>
-		/// <remarks>Similaire à l'extension méthode PrepareForRethrow présente dans System.CoreEx.dll du Reactive Framework</remarks>
-		public static Exception PrepForRemoting(this Exception self)
-		{
-			//TODO: cette extensions méthode est également présente dans System.CoreEx.dll du Reactive Framework!
-			// il faudra peut etre a terme rerouter vers cette version (si un jour Sioux reférence Rx directement...)
-			self = UnwrapIfAggregate(self);
-			s_preserveStackTrace?.Invoke(self, null);
-			return self;
-		}
-
 		/// <summary>Retourne la première exeception non-aggregate trouvée dans l'arbre des InnerExceptions</summary>
 		/// <param name="self">AggregateException racine</param>
 		/// <returns>Première exception dans l'arbre des InnerExceptions qui ne soit pas de type AggregateException</returns>
@@ -94,7 +54,7 @@ namespace Doxense.Tools
 		{
 			// dans la majorité des cas, on a une branche avec potentiellement plusieurs couches de AggEx mais une seule InnerException
 			var e = self.GetBaseException();
-			if (!(e is AggregateException)) return e;
+			if (e is not AggregateException) return e;
 
 			// Sinon c'est qu'on a un arbre a plusieurs branches, qu'on va devoir parcourir...
 			var list = new Queue<AggregateException>();
@@ -112,14 +72,6 @@ namespace Doxense.Tools
 			return self;
 		}
 
-		/// <summary>Retourne la première exception non-aggregate si c'est une AggregateException, ou l'exception elle même dans les autres cas</summary>
-		/// <param name="self"></param>
-		/// <returns></returns>
-		public static Exception UnwrapIfAggregate(this Exception self)
-		{
-			return self is AggregateException aggEx ? GetFirstConcreteException(aggEx) : self;
-		}
-
 		/// <summary>Rethrow la première exception non-aggregate trouvée, en jetant les autres s'il y en a</summary>
 		/// <param name="self">AggregateException racine</param>
 		[ContractAnnotation("self:null => null")]
@@ -127,17 +79,9 @@ namespace Doxense.Tools
 		public static Exception? Unwrap(
 			this AggregateException? self)
 		{
-			return self != null ? GetFirstConcreteException(self).PrepForRemoting() : null;
+			return self != null ? GetFirstConcreteException(self) : null;
 		}
 
-		/// <summary>Unwrap generic exceptions like <see cref="AggregateException"/> or <see cref="TargetInvocationException"/> to return the inner exceptions</summary>
-		public static Exception Unwrap(this Exception self)
-		{
-			if (self is AggregateException aggEx) return GetFirstConcreteException(aggEx);
-			if (self is TargetInvocationException tiEx) return tiEx.InnerException ?? self;
-			//add other type of "container" exceptions as required
-			return self;
-		}
 	}
 
 }
