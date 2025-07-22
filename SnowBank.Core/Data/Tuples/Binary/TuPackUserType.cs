5971da86003096ccc79f52bfcded3fd43a35956e
Christophe Chevalier <chevalier@snowbank-labs.com>
2025-07-22 12:25:57 +0200

    Core: implement ISpanFormattable on TuPackUserType, add SpecialKey helper

--- SnowBank.Core/Data/Tuples/Binary/TuPackUserType.cs
+++ SnowBank.Core/Data/Tuples/Binary/TuPackUserType.cs
@@ -30,62 +30,88 @@ namespace SnowBank.Data.Tuples.Binary
 	/// <summary>Represent a custom user type for the TuPack encoding</summary>
 	[DebuggerDisplay("{ToString()},nq")]
 	[PublicAPI]
-	public sealed class TuPackUserType : IEquatable<TuPackUserType>, IFormattable
+	public sealed class TuPackUserType : IEquatable<TuPackUserType>, IFormattable, ISpanFormattable
 	{
 
+		/// <summary>Prefix for the Directory Layer subspace (0xFE)</summary>
 		public const byte TypeDirectory = 0xFE;
 
+		/// <summary>Prefix for the System subspace (0xFF)</summary>
 		public const byte TypeSystem = 0xFF;
 
-		/// <summary>Directory Layer (<c>0xFE</c>)</summary>
+		/// <summary>Directory Layer (<c>`\xFE`</c>)</summary>
 		public static readonly TuPackUserType Directory = new(TypeDirectory);
 
-		/// <summary>System Subspace (<c>0xFF</c>)</summary>
+		/// <summary>Start of the System subspace (<c>`\xFF`</c>)</summary>
 		public static readonly TuPackUserType System = new(TypeSystem);
 
+		/// <summary>Start of the Special Key subspace (<c>`\xFF\xFF`</c>)</summary>
+		public static readonly TuPackUserType Special = new(TypeSystem, Slice.FromByte(255));
+
+		[SkipLocalsInit, MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public TuPackUserType(int type)
 		{
 			this.Type = type;
+			this.Value = Slice.Nil;
 		}
 
+		[SkipLocalsInit, MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public TuPackUserType(int type, Slice value)
 		{
 			this.Type = type;
 			this.Value = value;
 		}
 
+		/// <summary>Byte header of this custom user type</summary>
 		public readonly int Type;
 
+		/// <summary>Payload bytes of this custom user type (optional)</summary>
 		public readonly Slice Value;
 
-		public override string ToString()
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override string ToString() => ToString(null);
+
+		public string ToString(string? format, IFormatProvider? formatProvider = null)
+		{
+			return string.Create(formatProvider, $"{this}");
+		}
+
+		public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
 		{
 			switch (this.Type)
 			{
-				case TypeDirectory: return this.Value.IsNullOrEmpty ? "|Directory|" : $"|Directory|{this.Value:K}";
-				case TypeSystem: return this.Value.IsNullOrEmpty ? "|System|" : $"|System|{this.Value:K}";
+				case TypeDirectory: return this.Value.IsNull ? "|Directory|".TryCopyTo(destination, out charsWritten) : destination.TryWrite($"|Directory:{this.Value:K}|", out charsWritten);
+				case TypeSystem: return this.Value.IsNull ? "|System|".TryCopyTo(destination, out charsWritten) : destination.TryWrite($"|System:{this.Value:K}|", out charsWritten);
 			}
 
-			if (this.Value.IsNull)
-			{
-				return $"|User-{this.Type:X02}|";
-			}
-			return $"|User-{this.Type:X02}:{this.Value:N}|";
+			return this.Value.IsNull
+				? destination.TryWrite($"|User-{this.Type:X02}|", out charsWritten)
+				: destination.TryWrite($"|User-{this.Type:X02}:{this.Value:N}|", out charsWritten);
 		}
 
-		public string ToString(string? format, IFormatProvider? formatProvider) => ToString();
-
-		/// <summary>Returns a type that matches a system key (ex: <c>'\xFF/metadataVersion'</c>)</summary>
+		/// <summary>Returns a type that matches a System key (ex: <c>'\xFF/metadataVersion'</c>)</summary>
 		/// <param name="name">Key, excluding the initial <c>\xFF</c> byte (ex: "/metadataVersion" instead of "\xFF/metadataVersion")</param>
 		public static TuPackUserType SystemKey(Slice name) => new(TypeSystem, name);
 
-		/// <summary>Returns a type that matches a system key (ex: <c>'\xFF/metadataVersion'</c>)</summary>
+		/// <summary>Returns a type that matches a System key (ex: <c>'\xFF/metadataVersion'</c>)</summary>
 		/// <param name="name">Key, excluding the initial <c>\xFF</c> byte (ex: "/metadataVersion" instead of "\xFF/metadataVersion")</param>
 		public static TuPackUserType SystemKey(ReadOnlySpan<byte> name) => new(TypeSystem, name.ToSlice());
 
-		/// <summary>Returns a type that matches a system key (ex: <c>'\xFF/metadataVersion'</c>)</summary>
+		/// <summary>Returns a type that matches a System key (ex: <c>'\xFF/metadataVersion'</c>)</summary>
 		/// <param name="name">Name of key, excluding the initial <c>\xFF</c> byte (ex: "/metadataVersion" instead of "\xFF/metadataVersion")</param>
 		public static TuPackUserType SystemKey(string name) => new(TypeSystem, Slice.FromByteString(name));
+
+		/// <summary>Returns a type that matches a Special key (ex: <c>'\xFF\xFF/status/json'</c>)</summary>
+		/// <param name="name">Key, excluding the two initial <c>\xFF</c> bytes (ex: "/status/json" instead of "\xFF\xFF/status/json")</param>
+		public static TuPackUserType SpecialKey(Slice name) => new(TypeSystem, Slice.FromByte(255).Concat(name));
+
+		/// <summary>Returns a type that matches a Special key (ex: <c>'\xFF\xFF/status/json'</c>)</summary>
+		/// <param name="name">Key, excluding the two initial <c>\xFF</c> bytes (ex: "/status/json" instead of "\xFF\xFF/status/json")</param>
+		public static TuPackUserType SpecialKey(ReadOnlySpan<byte> name) => new(TypeSystem, Slice.FromByte(255).Concat(name));
+
+		/// <summary>Returns a type that matches a Special key (ex: <c>'\xFF\xFF/status/json'</c>)</summary>
+		/// <param name="name">Key, excluding the two initial <c>\xFF</c> bytes (ex: "/status/json" instead of "\xFF\xFF/status/json")</param>
+		public static TuPackUserType SpecialKey(string name) => new(TypeSystem, Slice.FromByteString("\xFF" + name));
 
 		/// <summary>Returns a custom user type with a given prefix (ex: <c>40</c>)</summary>
 		/// <param name="type">Prefix for this type, which must be between <see cref="TupleTypes.UserType0"/> (0x40) and <see cref="TupleTypes.UserTypeF"/> (0x4F)</param>
